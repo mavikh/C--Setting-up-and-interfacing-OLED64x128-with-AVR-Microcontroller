b96a434c6b129223cf286992cb393463ec26b0e2
mavikh <50956687+mavikh@users.noreply.github.com>
2019-10-12 12:37:32 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 18, 2019
+ *      Author: Vima
+ */
+
+//#include "OLED64x128.h"
+#include "i2c.h"
+
+ int main(){
+	 TWI_init(); //initiatei2c
+	 OLED_init();
+	 clear_screen();
+	// DDRB = (1<< PB0);	//set PB0 direction as output
+	// PORTB ^= (1<< PB0); //exclusive OR of pin with itself:Blink
+	 unsigned char *txt_ptr = " OLED 64x128 Set up.";//equal to char txt_ptr[] = "abcdefghijklmnopqrstuv";
+	 write_text(2,txt_ptr);
+	 txt_ptr = "    OLED is ready!";//equal to char txt_ptr[] = "abcdefghijklmnopqrstuv";
+	 write_text(5,txt_ptr);
+	 update_screen();
+	 
+	 return(0);
+ }
